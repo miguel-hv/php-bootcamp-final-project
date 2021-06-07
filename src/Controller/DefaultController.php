361a6716035c8ff567e0b43ce4f4426201f954fd
mhdelavega <huertadelavegamiguel@gmail.com>
2021-06-07 12:22:06 +0200

    FEAT: login and register routing

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -11,9 +11,24 @@ class DefaultController extends AbstractController {
     /**
      * @Route("/", name="homePage")
      */
-    public function homePage (): Response
+    public function homePage(): Response
     {
         return $this->render('Home/home.html.twig', []);
     }
 
+    /**
+     * @Route("/register", name="registerPage")
+     */
+    public function registerPage(): Response
+    {
+        return $this->render('Register/register.html.twig',[]);
+    }
+
+    /**
+     * @Route("/login", name="loginPage")
+     */
+    public function loginPage(): Response
+    {
+        return $this->render('Login/login.html.twig', []);
+    }
 }
